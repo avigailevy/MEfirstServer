8af6b1b04b0f8c20430d4f27672b54adabe00c32
avigailevy <avigailevy929@gmail.com>
2025-06-15 19:37:59 +0300

    Added a route for all todos

--- API/todosRoutes.js
+++ API/todosRoutes.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const createGenericRouter = require('./genericRouter');
+const genericServices = require('../Services/genericServices');
 const router = express.Router();
 
-const genericRouter = createGenericRouter('todos', 'todo_id', ['from_user_id', 'to_user_id', 'title']);
-router.use('/', genericRouter);
-
-// כאן תוכל להוסיף ניתובים נוספים בעתיד
+// ניתוב שמחזיר את כל המשימות של משתמש מסוים
+router.get('/:username/todos/all', async (req, res) => {
+    const {username}= req.params;
+    try{
+        const userId= genericServices.getRecordByColumn('users', 'user_id', `${username}`);
+        if (!userId) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        const todos = await genericServices.getAllRecords('todos', 'user_id', userId);
+        req.json(todos);
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 
 module.exports = router;
