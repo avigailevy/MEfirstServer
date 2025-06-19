ee9ece5bca3dacb7c5b852535008539e31c5583f
avigailevy <avigailevy929@gmail.com>
2025-06-19 19:14:02 +0300

    Fixed the route of agents & Added authenticateToken in usersRoutes

--- API/usersRoutes.js
+++ API/usersRoutes.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const genericServices = require('../Services/genericServices');
+const {authenticateToken} = require("./middleware/authMiddleware/authenticateToken");
 
 //ניתוב שמחזיר את כל הסוכנים
-router.get('/agents/all', async (req, res) => {
+router.get('/agents/all', authenticateToken, async (req, res) => {
     try {
-        res.json(genericServices.getAllRecordsByColumn('users', 'role', 'agent'));
+        const user = req.user;
+        // בדוק אם המשתמש הוא מנהל
+        if (user.role !== 'admin') {
+            return res.status(403).json({ error: 'Only admin can see all agents' });
+        }        
+        const agents = await genericServices.getAllRecordsByColumn('users', 'role', 'agent');
+        res.json(agents);
     }
     catch (err) {
         res.status(500).json({ error: err.message });
     }
-})
+});
 
 module.exports = router;
